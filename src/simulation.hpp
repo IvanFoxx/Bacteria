f9758eb01b5f4b0423ca9959e1371d5dce132336
adidvar <adidvar@gmail.com>
2023-08-26 21:53:50 +0300

    Begining of writting new architecture

--- src/simulation.hpp
+++ src/simulation.hpp
@@ -9,20 +9,26 @@ class Simulation {
  public:
   Simulation();
 
-  void PushBactery(Bacterium b);
-  void PushFood(const Food& bacterium);
+  // void PushBactery(Bacterium b);
+  // void PushFood(const Food& bacterium);
 
-  void Run(float delta_time);
-  void InitRandomly();
-  void InitNewGeneration();
+  // void Run(float delta_time);
+  // void InitRandomly();
+  // void InitNewGeneration();
 
   const Field& GetField() const;
+  void SetField(const Field& field);
+
+  void Start(float delta);
+  void Stop();
+
+  int Generation();
 
  private:
-  float from_last_food_spawn_ = 0;
-  float from_epoch_start = 0;
+  // float from_last_food_spawn_ = 0;
+  // float from_epoch_start = 0;
   Field field_;
-  std::vector<std::shared_ptr<Bacterium>> dead_;
+  // std::vector<std::shared_ptr<Bacterium>> dead_;
 };
 
 #endif
